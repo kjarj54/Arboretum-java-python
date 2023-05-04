b8e70d8c43602b03a884cb56c99d548c08ac77c8
Arauz <86898345+kjarj54@users.noreply.github.com>
2023-05-03 18:21:39 -0600

    Primer commit
    Nombre del proyecto provisional, de momento el pom se le tiene que hacer los ajustes y se creo el archivo para el servidor

---
+++ arboretum_karauz_aavila_dazofeifa/src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module cr.ac.una.arboretum_karauz_aavila_dazofeifa {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens cr.ac.una.arboretum_karauz_aavila_dazofeifa to javafx.fxml;
+    exports cr.ac.una.arboretum_karauz_aavila_dazofeifa;
+}
