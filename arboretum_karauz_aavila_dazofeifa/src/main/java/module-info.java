023965b3dc2cc97a863757730e5e4d8ead1c01cf
Arauz <86898345+kjarj54@users.noreply.github.com>
2023-05-08 19:23:42 -0600

    Primer commit

--- arboretum_karauz_aavila_dazofeifa/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module cr.ac.una.arboretum_karauz_aavila_dazofeifa {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens cr.ac.una.arboretum_karauz_aavila_dazofeifa to javafx.fxml;
-    exports cr.ac.una.arboretum_karauz_aavila_dazofeifa;
-}
